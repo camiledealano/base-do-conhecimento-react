daee18050530be17854086672a49e0069d69562e
crissipires <crissiano_pires@hotmail.com>
2023-11-30 20:00:00 -0300

    api

--- api-bc/src/routes/router.js
+++ api-bc/src/routes/router.js
@@ -4,6 +4,12 @@ const usersRouter = require("./users");
 const articlesRouter = require("./articles");
 const authRouter = require("./auth");
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 router.use("/", usersRouter);
 router.use("/", articlesRouter);
 router.use("/", authRouter);
